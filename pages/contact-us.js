9f6c728e6f4c40170b2906bb31aac9204a8b72c2
mr-spicysauce <mr_spicysauce@yahoo.com>
2023-05-12 13:00:38 +0100

    finnished the contact us page

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -1,29 +1,66 @@
+import { Facebook } from "@/components/icons/Facebook"
+import { Instagram } from "@/components/icons/Instagram"
+import { Twitter } from "@/components/icons/Twitter"
+import { YouTube } from "@/components/icons/YouTube"
 import MainHeader from "@/components/main-header"
 import { useState } from "react"
 
 export default function ContactUs() {
     const [menu_open, set_menu_open] = useState(false)
-    return(
+    return (
         <>
-
             <MainHeader />
 
             <main id="main-container">
-                <h1 id="contact-us">CONTACT US!!!</h1>
+                <h1 className="contact-us">CONTACT US!!!</h1>
                 <div id="contain-top">
-                    <div className="item">
-                        <img height={"64px"} width={"64px"} src="/assets/phone_emoji.png"></img>
-                        <h1>+44 1632 960424</h1>
+                    <div className="item-header">
+                        <h1 className="item-header">By Email</h1>
+
+                        <div className="item">
+                            <img
+                                height={"64px"}
+                                width={"64px"}
+                                src="/assets/mail.png"
+                            ></img>
+                            <h1>BandName@Gmail.com</h1>
+                        </div>
                     </div>
 
-                    <div className="item">
-                        <img height={"64px"} width={"64px"} src="/assets/mail.png"></img>
-                        <h1>BandName@Gmail.com</h1>
+                    <div className="item-header">
+                        <h1>By Phone</h1>
+
+                        <div className="item">
+                            <img
+                                height={"64px"}
+                                width={"64px"}
+                                src="/assets/phone_emoji.png"
+                            ></img>
+                            <h1>+44 1632 960424</h1>
+                        </div>
                     </div>
                 </div>
 
-                </main>
+                <h1 className="contact-us">Follow us on social media!</h1>
 
+                <div id="socials-container">
+                    <a href="https://www.instagram.com/">
+                        <Instagram />
+                    </a>
+
+                    <a href="https://www.twitter.com/">
+                        <Twitter />
+                    </a>
+
+                    <a href="https://www.facebook.com/">
+                        <Facebook />
+                    </a>
+
+                    <a href="https://www.youtube.com/">
+                        <YouTube />
+                    </a>
+                </div>
+            </main>
         </>
     )
 }
